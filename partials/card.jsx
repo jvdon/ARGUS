70ec6cfd24eee8c65d1b7c2ade025f8718aa6e4c
Zeyk <joaovictordonato2@gmail.com>
2024-03-01 00:08:04 -0300

    Changed focus

--- partials/card.jsx
+++ partials/card.jsx
@@ -3,17 +3,27 @@ import React from "react";
 import styles from '@/styles/card.module.css'
 
 export default class Card extends React.Component {
+    /**
+     * 
+     * @param {import("react").ComponentProps} props 
+     */
+    constructor(props) {
+        super(props);
+        this.props = props;
+        this.description = this.props.description ?? "DESCRIPTION"
+    }
+
     render() {
         return (
             <div className={styles.card}>
                 <header>
-                    <h1>Hello</h1>
+                    <h1>{this.props.title}</h1>
                 </header>
                 <main>
-                    <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam odit, voluptate cum dicta id minima architecto sint possimus, repudiandae aliquam illum. Illum similique exercitationem accusamus, dignissimos possimus assumenda. Sit, dolore!</h2>
+                    <h2>{this.description}</h2>
                 </main>
                 <footer>
-                    <h2>CARD</h2>
+                    <h2>META</h2>
                 </footer>
             </div>
         );
